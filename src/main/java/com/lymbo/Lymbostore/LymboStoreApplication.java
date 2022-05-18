0bbb3d6e1b2cfe84fbba030b2b74c9379d1e0697
Pushpendra Sahu <34132874+pksahu02@users.noreply.github.com>
2022-05-18 10:07:05 +0530

    Repository created with spring initializer

---
+++ src/main/java/com/lymbo/Lymbostore/LymboStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lymbo.Lymbostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LymboStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LymboStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lymbo/Lymbostore/LymboStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lymbo.Lymbostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LymboStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
